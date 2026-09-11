edf5017346f690df23558408ba9fb83bbf4cb6a1
Elfaria <elfaria@ctknet.ne.jp>
2026-09-11 19:21:00 +0900

    In-place updates for quotes: update QuoteViewModel properties instead of clearing collection; keep DisplayQuotes stable when possible

--- TickerBoard/MainViewModel.cs
+++ TickerBoard/MainViewModel.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows.Media;
 using System.Windows.Threading;
+using System.Linq;
 namespace TickerBoard;
 public sealed class MainViewModel : INotifyPropertyChanged
 {
